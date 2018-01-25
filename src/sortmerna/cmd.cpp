8e46600444df9f0d5c18d88863a2bcf21d56d48d
biocodz <biocodz@protonmail.com>
2018-01-25 08:28:46 -0500

    interactive prompt module

---
+++ src/sortmerna/cmd.cpp
@@ -0,0 +1,32 @@
+/* 
+ * FILE: cmd.cpp
+ * Created: Jan 17, 2018 Wed
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "cmd.hpp"
+#include "options.hpp"
+#include "kvdb.hpp"
+#include "read.hpp"
+
+void CmdSession::run(Runopts & opts)
+{
+	std::stringstream ss;
+	std::string cmd;
+	Read read;
+	KeyValueDatabase kvdb(opts.kvdbPath);
+
+	for (;;)
+	{
+		std::cout << "Enter command: [record number, exit]: ";
+		read.clear();
+		std::cin >> cmd;
+		if ("exit" == cmd) break;
+		read.init(opts, kvdb, std::stoi(cmd));
+		ss << "read.hit= " << read.hit << std::endl;
+		std::cout << ss.str(); ss.str("");
+	}
+} // ~CmdSession::run
