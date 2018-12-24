e4e1a22004ed12043c152966f961778c0853cb65
biocodz <biocodz@protonmail.com>
2018-12-24 08:36:04 -0500

    cmake install and packaging implemented

--- cmake/modules/FindRapidJson.cmake
+++ cmake/modules/FindRapidJson.cmake
@@ -7,17 +7,33 @@
 #include(FindPkgConfig)
 #find_package(PkgConfig)
 #pkg_check_modules(PC_RapidJSON QUIET RapidJSON)
+if (WIN32)
+    set(_sfx cmake)
+else()
+    set(_sfx lib/cmake/RapidJSON)
+endif()
+
+# RapidJson_DIR
+find_path(RapidJson_DIR
+    NAMES RapidJSONConfig.cmake
+    PATHS ${RAPIDJSON_HOME}/${_sfx}
+    NO_DEFAULT_PATH
+)
+message("RapidJson_DIR: ${RapidJson_DIR}")
+#set(RapidJson_DIR ${RAPIDJSON_HOME}/${_sfx} CACHE PATH "configuration files directory" FORCE)
 
 find_path(RapidJSON_INCLUDE_DIR
   NAMES rapidjson.h
   PATHS ${RAPIDJSON_HOME}/include
   PATH_SUFFIXES rapidjson
+  NO_DEFAULT_PATH
 )
 message("RapidJSON_INCLUDE_DIR: ${RapidJSON_INCLUDE_DIR}")
 
 # get version. It is defined in CMakeLists and put into RapidJSONConfig.cmake =>
 # extract version from RapidJSONConfigVersion.cmake
-file(STRINGS ${RAPIDJSON_HOME}/cmake/RapidJSONConfigVersion.cmake version_line REGEX "PACKAGE_VERSION \"")
+file(STRINGS ${RapidJson_DIR}/RapidJSONConfigVersion.cmake version_line REGEX "PACKAGE_VERSION \"")
+
 string(REGEX MATCH "([0-9]+\.[0-9]+\.[0-9]+)" _ ${version_line})
 message("version_line: ${version_line}")
 message("CMAKE_MATCH_0: ${CMAKE_MATCH_0}")
