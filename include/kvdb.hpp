ac57e8a87ddc6716e57b382ad5f08777fc8b27d1
biocodz <biocodz@protonmail.com>
2019-10-11 09:13:25 -0400

    moved func definitions to cpp file

--- include/kvdb.hpp
+++ include/kvdb.hpp
@@ -4,37 +4,21 @@
  * Created: Nov 06, 2017 Mon
  */
 
+#include  <iostream>
+
 #include "rocksdb/db.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/options.h"
 
 class KeyValueDatabase {
 public:
-	KeyValueDatabase(std::string kvdbPath) {
-		// init and open key-value database for read matches
-		options.IncreaseParallelism();
-#if defined(_WIN32)
-		options.compression = rocksdb::kXpressCompression;
-#else
-		options.compression = rocksdb::kZlibCompression;
-#endif
-		options.create_if_missing = true;
-		rocksdb::Status s = rocksdb::DB::Open(options, kvdbPath, &kvdb);
-		assert(s.ok());
-	}
+	KeyValueDatabase(const std::string& kvdbPath);
+
 	~KeyValueDatabase() { delete kvdb; }
 
-	void put(std::string key, std::string val)
-	{
-		rocksdb::Status s = kvdb->Put(rocksdb::WriteOptions(), key, val);
-	}
+	void put(std::string key, std::string val);
 
-	std::string get(std::string key)
-	{
-		std::string val;
-		rocksdb::Status s = kvdb->Get(rocksdb::ReadOptions(), key, &val);
-		return val;
-	}
+	std::string get(std::string key);
 
 	int clear(std::string dbPath);
 private:
--- src/sortmerna/kvdb.cpp
+++ src/sortmerna/kvdb.cpp
@@ -5,6 +5,32 @@
  */
 #include "kvdb.hpp"
 
+KeyValueDatabase::KeyValueDatabase(const std::string& kvdbPath) : kvdb(0)
+{
+	// init and open key-value database for read matches
+	options.IncreaseParallelism();
+#if defined(_WIN32)
+	options.compression = rocksdb::kXpressCompression;
+#else
+	options.compression = rocksdb::kZlibCompression;
+#endif
+	options.create_if_missing = true;
+	rocksdb::Status s = rocksdb::DB::Open(options, kvdbPath, &kvdb);
+	assert(s.ok());
+}
+
+void KeyValueDatabase::put(std::string key, std::string val)
+{
+	rocksdb::Status s = kvdb->Put(rocksdb::WriteOptions(), key, val);
+}
+
+std::string KeyValueDatabase::get(std::string key)
+{
+	std::string val;
+	rocksdb::Status s = kvdb->Get(rocksdb::ReadOptions(), key, &val);
+	return val;
+}
+
 /* 
  * Remove database files from the given location
  */
