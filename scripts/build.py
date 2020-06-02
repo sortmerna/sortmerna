d7cef43df025357a2d973965f1860815ccf8e855
biocodz <biocodz@protonmail.com>
2020-06-02 19:49:01 -0400

    pdb disabled

--- scripts/build.py
+++ scripts/build.py
@@ -593,7 +593,7 @@ if __name__ == "__main__":
     python /mnt/c/Users/biocodz/a01_code/sortmerna/scripts/build.py --name sortmerna
         --winhome /mnt/c/Users/biocodz --btype debug
     '''
-    import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
 
     # options
     optpar = OptionParser()
