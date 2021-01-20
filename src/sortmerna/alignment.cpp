4d4bfd76eaf0c91b99d396bd00af82b5e23785df
biocodz <biocodz@protonmail.com>
2021-01-20 10:20:14 -0500

    comment edited

--- src/sortmerna/alignment.cpp
+++ src/sortmerna/alignment.cpp
@@ -216,18 +216,17 @@ void compute_lis_alignment
 		//       It has 3 'break' instructions though. Convoluted.
 		while (hits_on_ref_iter != hits_on_ref.end() && is_search_candidates)
 		{
-			/*
-			* max possible k-mer start position: 
-			*   max start position on the reference of a matching k-mer for the read 
-			*   that is overlaid/anchored on the reference using a matching k-mer
-			*
-			* ref: |--------|k-mer anchor|-------|k-mer|--------------------|k-mer|-----|
-			*               ^begin_ref e.g. 20                              ^end_ref
-			* read:    |----|k-mer anchor|----|k-mer|----------|k-mer|----|---|
-			*          |    ^begin_read e.g 10                 ^end_read  |___|_ lnwin
-			*          ^read start pos                                    ^   ^read end pos
-			*                                                             |_max possible k-mer start position 'end_ref_max'
-			*/
+			// max possible k-mer start position: 
+			//   max start position on the reference of a matching k-mer for the read 
+			//   that is overlaid/anchored on the reference using a matching k-mer
+			// 
+			// ref: |--------|k-mer anchor|-------|k-mer|--------------------|k-mer|-----|
+			//               ^begin_ref e.g. 20                              ^end_ref
+			// read:    |----|k-mer anchor|----|k-mer|----------|k-mer|----|---|
+			//          |    ^begin_read e.g 10                 ^end_read  |___|_ lnwin
+			//          ^read start pos                                    ^   ^read end pos
+			//                                                             |_max possible k-mer start position 'end_ref_max'
+			// 
 			auto end_ref_max = begin_ref - begin_read + read.sequence.length() - refstats.lnwin[index.index_num];
 			//uint32_t end_ref = begin_ref + read.sequence.length() - refstats.lnwin[index.index_num] + 1; // TODO: original - wrong?
 			bool push = false;
