d3916111dd77b553cdc0bc402c321f45696291fa
cmith44 <nathaniel.smith44@hotmail.com>
2023-06-20 08:25:34 +0000

    "Moved conf.py back"
    This reverts commit e309a98ffa4de6e37db030e20093083e8f524801.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 pygments_style = 'sphinx'
 
+master_doc = 'index'
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
