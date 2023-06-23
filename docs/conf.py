7b0a7fa3d529270673121fb3d10751f7ac76b5ee
cmith44 <nathaniel.smith44@hotmail.com>
2023-06-23 09:59:44 +0000

    Moved conf.py

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import sys
+import os
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sortmerna'
+copyright = '2022, sortmerna development team'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+pygments_style = 'sphinx'
+
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'default'
+try:
+	from better import better_theme_path
+	html_theme_path = [better_theme_path]
+	html_theme = 'better'
+except ImportError:
+	pass
+	
+html_static_path = ['_static']
+html_use_smartypants = True
